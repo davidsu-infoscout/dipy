abdd7237c6f4fdb51b6cab2ecd07a5463745261c
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-10-09 17:24:54 -0400

    minor update

--- dipy/bundle/scripts/warp_t1_b0.py
+++ dipy/bundle/scripts/warp_t1_b0.py
@@ -67,7 +67,7 @@ fname2_mask = join(dname, 'dwi_nlm_mask_1x1x1.nii.gz')
 nib.save(nib.Nifti1Image(mask2.astype(np.uint8), affine2), fname2_mask)
 
 fname2_S0 = join(dname, 'dwi_nlm_S0_1x1x1.nii.gz')
-nib.save(nib.Nifti1Image(data2[..., b0_index], affine2), fname2_mask)
+nib.save(nib.Nifti1Image(data2[..., b0_index], affine2), fname2_S0)
 
 fname2_slice = 'dwi_nlm_slice_1x1x1.png'
 record_slice(fname2_slice, data2[..., b0_index], k=data2.shape[2]/2)
@@ -82,38 +82,35 @@ fname2_fa = join(dname, 'dwi_nlm_fa_1x1x1.nii.gz')
 nib.save(nib.Nifti1Image(tenfit.fa, affine2), fname2_fa)
 
 fname_slice_fa = 'dwi_nlm_slice_FA_1x1x1.png'
-record_slice(fname_slice_fa, tenfit.fa, k=data.shape[2]/2)
+record_slice(fname_slice_fa, tenfit.fa, k=data2.shape[2]/2)
 
 print('>>> 5. Warp T1 to S0 using ANTS...')
 
-# curr_dname = dirname(realpath(__file__))
-# print(curr_dname)
-
-fT1 = join(dname, 't1_brain.nii.gz')
+fT1 = join(dname, 't1.nii.gz')
 fS0 = join(dname, 'dwi_nlm_S0_1x1x1.nii.gz')
 fFA = join(dname, 'dwi_nlm_fa_1x1x1.nii.gz')
 fT1wS0 = join(dname, 't1_brain_warped_S0.nii.gz')
-fT1wTrans = join(dname, 't1_brain_nonlinear_transform.nii.gz')
+fdef = join(dname, 'MultiVar')
 
-1/0
+img_T1 = nib.load(fT1)
+print(img_T1.get_data().shape)
+print(img_T1.get_affine())
+print(nib.aff2axcodes(img_T1.get_affine()))
+
+del img_T1
 
 from dipy.external.fsl import pipe
 
-#cmd0 = "cd " + dname
+# br1 = "[" + fS0 + ", " + fT1 + ", 1, 4]"
+# br2 = "[" + fFA + ", " + fT1 + ", 1.5, 4]"
 
-br1 = "[" + fS0 + ", " + fT1 + ", 1, 4]"
-br2 = "[" + fFA + ", " + fT1 + ", 1.5, 4]"
+# cmd1 = "ANTS 3 -m CC" + br1 + " -m CC" + br2 + " -o MultiVar -i 75x75x10 -r Gauss[3,0] -t SyN[0.25]"
+# cmd2 = "WarpImageMultiTransform 3 " + fT1 + " " + fT1wS0 + " -R " + fS0 + " " + fdef + "Warp.nii.gz " + fdef + "Affine.txt"
 
-cmd1 = "ANTS 3 -m CC" + br1 + " -m CC" + br2 + " -o MultiVar -i 75x75x10 -r Gauss[3,0] -t SyN[0.25]"
+# print(cmd1)
+# pipe(cmd1)
+# print(cmd2)
+# pipe(cmd2)
 
-#cmd2 = "ANTS 3 -m CC[b0_brain.nii.gz,t1_crop.nii.gz,1,4] -m CC[fa.nii.gz,t1_crop.nii.gz,1.5,4] -o MultiVar -i 75x75x10 -r Gauss[3,0] -t SyN[0.25]"
-#cmd3 = "WarpImageMultiTransform 3 t1_brain_on_upsamp_b0_brain.nii.gz t1_brain_nonlin_transform.nii.gz -R b0_brain.nii.gz MultiVarWarp.nii.gz MultiVarAffine.txt"
+print('>>> 6. Use freesurfer to create the labels...')
 
-cmd2 = "WarpImageMultiTransform 3 " + fT1wS0 + " " + fT1wTrans + " -R " + fS0 + " MultiVarWarp.nii.gz MultiVarAffine.txt"
-
-#cmd3 = "cd " + curr_dname
-
-#pipe(cmd0)
-pipe(cmd1)
-pipe(cmd2)
-#pipe(cmd3)
