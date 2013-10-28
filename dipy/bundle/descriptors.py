125ca1ae1cb30590fb816494ce9e45c1fdb2a2e1
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-10-28 15:13:55 -0400

    TEST: adding tests for descriptors

--- dipy/bundle/descriptors.py
+++ dipy/bundle/descriptors.py
@@ -12,13 +12,6 @@ from dipy.tracking.metrics import (downsample,
 from dipy.tracking.distances import cut_plane
 
 
-def fornix_streamlines():
-    fname = get_data('fornix')
-    streams, hdr = tv.read(fname)
-    streamlines = [i[0] for i in streams]
-    return streamlines
-
-
 def show_streamlines(streamlines, mpoints=None, cpoints=None):
     ren = fvtk.ren()
     fvtk.add(ren, fvtk.line(streamlines, line_colors(streamlines)))
@@ -34,18 +27,59 @@ def show_streamlines(streamlines, mpoints=None, cpoints=None):
 
 
 def length_distribution(streamlines):
+    """ Length distribution
+
+    Parameters
+    ----------
+    streamlines : list
+        List of ndarrays (N, 3)
+
+    Returns
+    -------
+    lengths : ndarray
+    """
+    if not isinstance(streamlines, list):
+        streamlines = [streamlines]
     lengths = length(streamlines)
     lengths = list(lengths)
-    return lengths
+    return np.array(lengths)
 
 
 def winding_angles(streamlines):
+    """ Total turning angle projected.
+
+    Project space curve to best fitting plane. Calculate the cumulative signed
+    angle between each line segment and the previous one.
+
+    Parameters
+    ----------
+    streamlines : list
+        List of arrays representing x,y,z of N points in a track.
+
+    Returns
+    -------
+    winds : ndarray
+        Total turning angles in degrees for every streamline.
+    """
     if not isinstance(streamlines, list):
         streamlines = [streamlines]
-    return [winding(s) for s in streamlines]
+    return np.array([winding(s) for s in streamlines])
 
 
 def avg_streamline(streamlines, pts=18):
+    """ Average streamline in set of streamlines
+    
+    Parameters
+    ----------
+    streamlines : list
+        List of arrays representing x,y,z of N points in a streamline.
+
+    Returns
+    -------
+    avg : ndarray        
+    """
+    if not isinstance(streamlines, list):
+        streamlines = [streamlines]
     if pts is None:
         return np.mean(streamlines, axis=0)
     else:
@@ -54,53 +88,85 @@ def avg_streamline(streamlines, pts=18):
 
 
 def qb_centroids(streamlines, thr=10, pts=18):
+    """ QuickBundles centroids
+
+    See also
+    --------
+    dipy.segment.quickbundles.QuickBundles
+    """
     qb = QuickBundles(streamlines, thr, pts)
     return qb.centroids
 
 
 def midpoints(streamlines):
+    """ Midpoint of each streamline
+    
+    Parameters
+    ----------
+    streamlines : list
+        List of ndarrays (N, 3)
+
+    Returns
+    -------
+    mpoints : ndarray
+        Array shape (M, 3) where M the number of streamlines
+    """
     if not isinstance(streamlines, list):
         streamlines = [streamlines]
     return np.array([midpoint(s) for s in streamlines])
 
 
 def centers_of_mass(streamlines):
+    """ Center of mass of each streamline
+    
+    Parameters
+    ----------
+    streamlines : list
+        List of ndarrays (N, 3)
+
+    Returns
+    -------
+    cpoints : ndarray
+        Array shape (M, 3) where M the number of streamlines
+    """
     if not isinstance(streamlines, list):
         streamlines = [streamlines]
     return np.array([center_of_mass(s) for s in streamlines])
 
 
-def dragons_hits(streamlines, avg_streamline):
+def _dragons_hits(streamlines, avg_streamline):
+    if not isinstance(streamlines, list):
+        streamlines = [streamlines]
     hits = cut_plane(streamlines, avg_streamline)
     xyz = [h[:, :3] for h in hits]
     angles = [h[:, 3:] for h in hits]
     return xyz, angles
 
 
-def dragons_hits_asarray(streamlines, avg_streamline):
-    xyz, angles = dragons_hits(streamlines, avg_streamline)
+def dragons_hits(streamlines, ref_streamline):
+    """ Intersections of streamlines by planes defined
+    along a reference streamline.
+    
+    Parameters
+    ----------
+    streamlines : list
+        List of ndarrays (N, 3)
+
+    ref_streamline : ndarray
+        Array of shape (K, 3)
+
+    Returns
+    -------
+    hpoints : ndarray
+        Array shape (M, 3) where M the number of hit points of the
+        streamlines to the planes
+    angles : ndarray
+        Array shape (M, 2) with the deviation polar angles from each
+        streamline to the reference streamline on the planes.
+    """
+    xyz, angles = _dragons_hits(streamlines, ref_streamline)
     return np.concatenate(xyz), np.concatenate(angles)
 
 
-streamlines = fornix_streamlines()
 
-lengths = length(streamlines)
-
-avg = avg_streamline(streamlines)
-
-centroids = qb_centroids(streamlines, 10)
-
-winds = winding_angles(centroids)
-
-mpoints = midpoints(centroids)
-
-cpoints = centers_of_mass(centroids)
-
-hpoints, hangles = dragons_hits_asarray(centroids, avg)
-
-ren = show_streamlines(centroids, mpoints)
-
-fvtk.add(ren, fvtk.point(hpoints, fvtk.colors.red))
-
-fvtk.show(ren)
 
