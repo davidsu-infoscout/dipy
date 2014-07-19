b5bd9963f83d40f9bb842363c2f71335aaaa2cb3
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2014-07-18 22:42:42 -0400

    Shore works but CSD doesn't

--- doc/examples/reconst_shore.py
+++ doc/examples/reconst_shore.py
@@ -29,20 +29,20 @@ def save_trk(streamlines, img, filename):
     nib.trackvis.write(shore_sl_fname, shore_streamlines_trk, hdr, points_space='voxel')
 
 
-def fvtk_gradients(gtab, radius, out_path, n_frames=1):
+def fvtk_gradients(gtab, radius):
 
     ren = fvtk.ren()
     fvtk.add(ren, fvtk.point(gtab.gradients, fvtk.colors.red, point_radius=radius))
     fvtk.show(ren)
-    fvtk.record(ren, out_path, n_frames=n_frames)
+    #fvtk.record(ren, out_path, n_frames=n_frames)
 
 
-def fvtk_bvecs(bvecs, radius, out_path, n_frames=1):
+def fvtk_bvecs(bvecs, radius):
 
     ren = fvtk.ren()
     fvtk.add(ren, fvtk.point(bvecs, fvtk.colors.red, point_radius=radius))
     fvtk.show(ren)
-    fvtk.record(ren, out_path, n_frames=n_frames)
+    #fvtk.record(ren, out_path, n_frames=n_frames)
 
 
 def dipy_to_fibernav_peaks(peaks):
@@ -51,7 +51,7 @@ def dipy_to_fibernav_peaks(peaks):
     return peaks_dirs
 
 
-def eudx_streamlines(stopping_volume, stopping_threshold, 
+def eudx_streamlines(stopping_volume, stopping_threshold,
                      peaks, seeds, sphere, final_number):
 
     stopping_values = np.zeros(peaks.peak_values.shape)
@@ -65,23 +65,23 @@ def eudx_streamlines(stopping_volume, stopping_threshold,
                                 a_low=stopping_threshold)
 
     return [streamline for streamline in streamline_generator][:final_number]
-   
+
 
 if __name__ == '__main__':
-    
+
     dname = '/home/eleftherios/Data/Alessandro_Crimi/data/'
     fdwi = dname + 'dwi1_fsl.nii.gz'
     fbval = dname + 'bvals.txt'
     fbvec = dname + 'bvec.txt'
     seeds = 10**6
     stopping_threshold = 0.1
-    denoise = True
+    denoise = False
     reconst_shore = True
     reconst_csd = False
 
     final_number = 400000
     fname_ending = '_crimi'
-    
+
     print('Loading dataset ...')
 
     bvecs = np.loadtxt(fbvec)
@@ -91,10 +91,12 @@ if __name__ == '__main__':
     img = nib.load(fdwi)
     data = img.get_data()
     affine = img.get_affine()
-    
+
     print('data.shape (%d, %d, %d, %d)' % data.shape)
 
     _, mask = median_otsu(data, 4, 4)
+
+    #mask = data[..., 0] > 50
 
     save_nifti(mask, affine, np.uint8, 'mask' + fname_ending + '.nii.gz')
 
@@ -103,7 +105,7 @@ if __name__ == '__main__':
     print('Starting nlmeans...')
 
     if denoise:
-        
+
         data = nlmeans(data, sigma=sigma/3., mask=mask)
 
     tensor_model = TensorModel(gtab)
@@ -119,7 +121,7 @@ if __name__ == '__main__':
     if reconst_shore:
         print('Starting Shore fitting and peaks...')
 
-        shore_model = ShoreModel(gtab, constrain_e0=True)        
+        shore_model = ShoreModel(gtab, constrain_e0=True)
 
         shore_peaks = peaks_from_model(model=shore_model,
                                        data=data,
@@ -128,16 +130,16 @@ if __name__ == '__main__':
                                        relative_peak_threshold=.5,
                                        min_separation_angle=25,
                                        parallel=True)
-       
 
-        save_nifti(dipy_to_fibernav_peaks(shore_peaks), affine, 
+
+        save_nifti(dipy_to_fibernav_peaks(shore_peaks), affine,
                    np.float32, 'shore_dirs_fibernav' + fname_ending + '.nii.gz')
 
         print('Saving Shore ODFs as SH ...')
-        
+
         save_nifti(shore_peaks.shm_coeff, affine, np.float32, 'SH' + fname_ending + '.nii.gz')
 
-        streamlines = eudx_streamlines(tensor_fit.fa, 0.1, 
+        streamlines = eudx_streamlines(tensor_fit.fa, 0.1,
                                        shore_peaks, seeds, sphere, final_number)
 
         save_trk(streamlines, img, 'shore_streamlines' + fname_ending + '.trk')
@@ -158,16 +160,16 @@ if __name__ == '__main__':
                                     min_separation_angle=25,
                                     parallel=True)
 
-        save_nifti(dipy_to_fibernav_peaks(csd_peaks), affine, 
+        save_nifti(dipy_to_fibernav_peaks(csd_peaks), affine,
                    np.float32, 'csd_dirs_fibernav' + fname_ending + '.nii.gz')
 
         print('Saving CSD ODFs as SH ...')
-        
+
         save_nifti(csd_peaks.shm_coeff, affine, np.float32, 'csd_SH' + fname_ending + '.nii.gz')
 
-        streamlines = eudx_streamlines(tensor_fit.fa, 0.1, 
+        streamlines = eudx_streamlines(tensor_fit.fa, 0.1,
                                        csd_peaks, seeds, sphere, final_number)
-        
+
         save_trk(streamlines, img, 'csd_streamlines' + fname_ending + '.trk')
 
 
