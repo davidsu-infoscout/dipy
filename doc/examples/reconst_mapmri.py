c40990bc836ed7f64064df31b8d71956ef05230f
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2014-11-01 21:52:45 -0400

    Changed tutorial to work with real data

--- doc/examples/reconst_mapmri.py
+++ doc/examples/reconst_mapmri.py
@@ -12,13 +12,16 @@ First import the necessary modules:
 
 from dipy.reconst.mapmri import MapmriModel
 from dipy.viz import fvtk
-from dipy.data import fetch_isbi2013_2shell, read_isbi2013_2shell, get_sphere
+#from dipy.data import fetch_isbi2013_2shell, read_isbi2013_2shell, get_sphere
+from dipy.data import (fetch_sherbrooke_3shell, read_sherbrooke_3shell,
+                       get_sphere)
+
 from dipy.core.gradients import gradient_table
 
 """
 Download and read the data for this tutorial.
 
-fetch_isbi2013_2shell() provides data from the ISBI HARDI contest 2013 acquired 
+fetch_isbi2013_2shell() provides data from the ISBI HARDI contest 2013 acquired
 for two shells at b-values 1500 and 2500.
 
 The six parameters of these two functions define the ROI where to reconstruct
@@ -26,10 +29,19 @@ the data. They respectively correspond to (xmin,xmax,ymin,ymax,zmin,zmax)
 with x, y, z and the three axis defining the spatial positions of the voxels.
 """
 
-fetch_isbi2013_2shell()
-img, gtab = read_isbi2013_2shell()
+fetch_sherbrooke_3shell()
+img, gtab = read_sherbrooke_3shell()
+
+#fetch_isbi2013_2shell()
+#img, gtab = read_isbi2013_2shell()
+
 data = img.get_data()
-data_small = data[10:40, 22, 10:40]
+
+
+data_small = data[:, 58:59, :]
+mask_small = data_small[..., 0] > 100
+
+#data_small = data[10:40, 22, 10:40]
 
 print('data.shape (%d, %d, %d, %d)' % data.shape)
 
@@ -52,7 +64,7 @@ map_model = MapmriModel(gtab, radial_order=radial_order)
 Fit the MAPMRI model to the data
 """
 
-mapfit = map_model.fit(data_small)
+mapfit = map_model.fit(data_small, mask_small)
 
 """
 Load an odf reconstruction sphere
@@ -65,24 +77,28 @@ Compute the ODFs
 """
 
 odf = mapfit.odf(sphere)
-print('odf.shape (%d, %d, %d)' % odf.shape)
+print('odf.shape (%d, %d, %d, %d)' % odf.shape)
 
 """
 Display the ODFs
 """
 
 r = fvtk.ren()
-sfu = fvtk.sphere_funcs(odf[:, None, :], sphere, colormap='jet')
-sfu.RotateX(-90)
+sfu = fvtk.sphere_funcs(odf, sphere, colormap='jet')
+
 fvtk.add(r, sfu)
-fvtk.record(r, n_frames=1, out_path='odfs.png', size=(600, 600))
+fvtk.show(r)
+
+#sfu.RotateX(-90)
+#fvtk.add(r, sfu)
+#fvtk.record(r, n_frames=1, out_path='odfs.png', size=(600, 600))
 
 """
 .. figure:: odfs.png
    :align: center
 
    **Orientation distribution functions**.
-   
+
 .. [Ozarslan2013] Ozarslan E. et. al, "Mean apparent propagator (MAP) MRI: A novel
                diffusion imaging method for mapping tissue microstructure",
                NeuroImage, 2013.
