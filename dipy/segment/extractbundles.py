571fbc1975d362fae66b1b63b638f40326181f13
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2014-11-03 13:47:47 -0500

    Updated ExtractBundles with find still testing it

--- dipy/segment/extractbundles.py
+++ dipy/segment/extractbundles.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 from dipy.align.streamlinear import StreamlineLinearRegistration
-from dipy.tracking.streamline import length
+from dipy.tracking.streamline import (length, transform_streamlines,
+                                      set_number_of_points)
 from dipy.segment.quickbundles import QuickBundles
+from dipy.tracking.distances import bundles_distances_mdf
 from time import time
 
 
@@ -43,16 +45,64 @@ def whole_brain_registration(streamlines1, streamlines2,
 
     moved_streamlines2 = slm.transform(streamlines2)
 
-    return moved_streamlines2, slm.matrix
+    return moved_streamlines2, slm.matrix, qb_centroids1, qb_centroids2
+
+
+def find_bundle(model_bundle_moved, streamlines2, min_thr=10):
+
+    model_bundle_moved = set_number_of_points(model_bundle_moved, 20)
+    streamlines2 = set_number_of_points(streamlines2, 20)
+
+    D = bundles_distances_mdf(model_bundle_moved, streamlines2)
+
+    D[D > min_thr] = np.inf
+
+    print(D.shape)
+
+    indices = np.argmin(D, axis=1)
+
+    return [streamlines2[index] for (i, index) in enumerate(indices) if D[i, index] != np.inf]
+
+
+def find_bundle2(model_bundle_moved, streamlines2, min_thr=10):
+
+    model_bundle_moved = set_number_of_points(model_bundle_moved, 20)
+    streamlines2 = set_number_of_points(streamlines2, 20)
+
+    D = bundles_distances_mdf(model_bundle_moved, streamlines2)
+
+    D[D > min_thr] = np.inf
+
+    print(D.shape)
+
+    mins = np.min(D, axis=0)
+
+    return [streamlines2[i] for i in np.where(mins!=np.inf)[0]]
+
 
 
 class ExtractBundles():
     """ Extract bundles from whole brain streamlines using an initial
     model of the streamlines """
 
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        self.qb_model_streamlines = None
+        self.qb_streamlines = None
+        self.moved_model_streamlines = None
+
     def extract(self, streamlines, model_streamlines, model_bundle):
 
-        moved_model_streamlines, mat = whole_brain_registration(streamlines,
-                                                                model_streamlines)
+        ret = whole_brain_registration(streamlines, model_streamlines)
+        moved_model_streams, mat, qb_models_streamlines, qb_model = ret
 
-        return moved_model_streamlines
+        self.moved_model_streamlines = moved_model_streams
+        self.qb_models_streamlines = qb_models_streamlines
+        self.qb_model = qb_model
+
+        moved_model_bundle = transform_streamlines(model_bundle, mat)
+        self.moved_model_bundle = moved_model_bundle
+
+        find_bundle()
+
+
