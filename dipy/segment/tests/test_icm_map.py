93c00d7a9d3d822f36c131eb482410030ce7e038
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-06-27 17:49:05 -0400

    Playing with clustering

--- dipy/segment/tests/test_icm_map.py
+++ dipy/segment/tests/test_icm_map.py
@@ -21,6 +21,52 @@ masked_img = applymask(dataimg, datamask)
 print('masked_img.shape (%d, %d, %d)' % masked_img.shape)
 shape = masked_img.shape[:3]
 
+
+def cluster_t1(data, dist_thr=20.):
+
+    labels = np.zeros_like(data)
+
+
+    cluster_centroids = [0]
+    cluster_sizes = [1]
+    cluster_labels = [0]
+
+    for idx in ndindex(data.shape):
+        print(idx)
+
+        close_centroid = 0
+
+        for (centroid, size, label) in zip(cluster_centroids, cluster_sizes, cluster_labels):
+
+            if np.abs(data[idx] - centroid / np.float(size)) <= dist_thr:
+
+                labels[idx] = label
+                cluster_centroids[label] += data[idx]
+                cluster_sizes[label] += 1
+                close_centroid = 1
+
+        if close_centroid == 0 :
+
+            label = len(cluster_labels)
+            cluster_labels.append(label)
+
+            labels[idx] = label
+            cluster_centroids.append(data[idx])
+            cluster_sizes.append(1)
+
+
+        # print(cluster_sizes)
+        # print(cluster_labels)
+        # print(cluster_centroids)
+
+    return labels, cluster_centroids, cluster_sizes, cluster_labels
+
+data = masked_img[:, :, 90:92]
+labels, centroids, sizes, label_indices = cluster_t1(data, 40.)
+
+1/0
+
+
 seg = nib.load(dname + '3587_BL_T1_to_MNI_Linear_6p_seg.nii.gz')
 seg_init = seg.get_data()
 seg_init_masked = applymask(seg_init, datamask)
